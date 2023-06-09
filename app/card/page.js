aed2f562681b52c221d88a972be0efbaba6e3fc3
Priyankachikhle <100230185+Priyankachikhle@users.noreply.github.com>
2023-06-09 16:50:19 +0530

    Update page.js

--- app/card/page.js
+++ app/card/page.js
@@ -1,10 +1,10 @@
 import React from "react";
 import styles from "./page.module.css";
 
-const Card = () => {
+const Card = () => (
     <div>
         <h1>Card</h1>
     </div>
-};
+);
 
 export default Card;
